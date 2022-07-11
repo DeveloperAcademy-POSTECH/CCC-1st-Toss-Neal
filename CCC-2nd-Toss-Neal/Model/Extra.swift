0d75ddded844216e2fe9899591ff47aa0f57ec83
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-07-11 20:41:52 +0900

    [Fix]  ViewModel과 Model로 구분, 추후 Model을 전체 적용되는 구조로 변경할 필요.

---
+++ CCC-2nd-Toss-Neal/Model/Extra.swift
@@ -0,0 +1,14 @@
+//
+//  CellData.swift
+//  CCC-2nd-Toss-Neal
+//
+//  Created by yudonlee on 2022/07/05.
+//
+
+import Foundation
+
+struct Extra {
+    var subtitle: String
+    var mainTitle: String
+    var imageName: String
+}
