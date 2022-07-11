a0e8c39816c744a7938948e31d039cca96026dc5
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-07-11 20:34:08 +0900

    [Fix] UICollectionView identifier static 으로 지정

--- CCC-2nd-Toss-Neal/Controllers/Core/HomeViewController.swift
+++ CCC-2nd-Toss-Neal/Controllers/Core/HomeViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    private let dummyData: [Extra] = [Extra(subtitle: "최대 15만원", mainTitle: "카드 혜택받기 ", imageName: "card"), Extra(subtitle: "요즘 인기", mainTitle: "오늘의 머니팁", imageName: "light"), Extra(subtitle: "집 있다면", mainTitle: "내 부동산 시세조회", imageName: "house"), Extra(subtitle: "인기", mainTitle: "더보기", imageName: "")]
+    
     private let inquiryCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: 120, height: 120)
@@ -15,17 +17,28 @@ class HomeViewController: UIViewController {
 //        왜 zero를 해주는걸까?
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.showsHorizontalScrollIndicator = false
-        collectionView.register(InquiryCollectionViewCell.self, forCellWithReuseIdentifier: "inquiryCell")
-        collectionView.contentInset = UIEdgeInsets(top: 0, left: 10, bottom:0, right: 0)
+        collectionView.register(InquiryCollectionViewCell.self, forCellWithReuseIdentifier: InquiryCollectionViewCell.identifier)
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        
         return collectionView
     }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .tossBackgroundColor
+        setupColor()
         setupMainLayout()
+        setupNavigationItem()
+    }
+    
+    private func setupColor() {
+        self.view.backgroundColor = .tossBackgroundColor
+    }
+    
+    private func setupNavigationItem() {
         self.navigationItem.leftBarButtonItem = makeleftBaritem()
         self.navigationItem.rightBarButtonItems = makeRightBaritems()
     }
+    
     private func setupMainLayout() {
         view.addSubview(inquiryCollectionView)
         inquiryCollectionView.delegate = self
@@ -53,28 +66,34 @@ extension HomeViewController {
             let item = UIBarButtonItem(customView: button)
             return item
         }()
+        
         return tossLeftTitle
     }
+    
     private func makeRightBaritems() -> [UIBarButtonItem] {
         let addButton: UIBarButtonItem = {
             let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(buttonPressed(_:)))
             button.tintColor = .tossTintColor
             return button
         }()
+        
         let bellButton: UIBarButtonItem = {
             let imgIcon = UIImage(systemName: "bell.fill")
             let button = UIBarButtonItem(image: imgIcon, style: .plain, target: self, action: #selector(buttonPressed(_:)))
             button.tintColor = .tossTintColor
             return button
         }()
+        
         let chatButton: UIBarButtonItem = {
             let imgIcon = UIImage(systemName: "message.fill")
             let button = UIBarButtonItem(image: imgIcon, style: .plain, target: self, action: #selector(buttonPressed(_:)))
             button.tintColor = .tossTintColor
             return button
         }()
+        
         return [bellButton, chatButton, addButton]
     }
+    
     @objc private func buttonPressed(_ sender: Any) {
         print("Button pressed")
     }
@@ -85,10 +104,13 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
         return dummyData.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "inquiryCell", for: indexPath) as? InquiryCollectionViewCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: InquiryCollectionViewCell.identifier, for: indexPath) as? InquiryCollectionViewCell else {
             fatalError()
         }
-        cell.configure(dummyData[indexPath.row])
+        
+        let model = dummyData[indexPath.row]
+        cell.configure(with: InquiryViewModel(subtitle: model.subtitle, mainTitle: model.mainTitle, imageName: model.imageName))
+        
         cell.backgroundColor = .white
         cell.layer.cornerRadius = 15.0
         return cell
--- CCC-2nd-Toss-Neal/View/InquiryCollectionViewCell.swift
+++ CCC-2nd-Toss-Neal/View/InquiryCollectionViewCell.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class InquiryCollectionViewCell: UICollectionViewCell {
+    static let identifier = "InquiryCollectionViewCell"
+    
     private let subTitle: UILabel = {
         let lbl = UILabel()
         lbl.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
@@ -15,6 +17,7 @@ class InquiryCollectionViewCell: UICollectionViewCell {
         lbl.textAlignment = .left
         return lbl
     }()
+    
     private let mainTitle: UILabel = {
         let lbl = UILabel()
         lbl.font = UIFont.systemFont(ofSize: 14, weight: .bold)
@@ -24,6 +27,7 @@ class InquiryCollectionViewCell: UICollectionViewCell {
         lbl.lineBreakMode = .byWordWrapping
         return lbl
     }()
+    
     private let imageIcon: UIImageView = {
         let imgView = UIImageView()
         imgView.contentMode = .scaleAspectFill
@@ -31,56 +35,34 @@ class InquiryCollectionViewCell: UICollectionViewCell {
         imgView.backgroundColor = .clear
         return imgView
     }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.initView()
     }
+    
     required init(coder: NSCoder) {
         fatalError("init error")
     }
+    
     func initView() {
         self.contentView.backgroundColor = .clear
-        for item in [subTitle, mainTitle, imageIcon] {
-            self.contentView.addSubview(item)
+        
+        [subTitle, mainTitle, imageIcon].forEach { view in
+            self.contentView.addSubview(view)
         }
+        
         subTitle.anchor(top: contentView.safeAreaLayoutGuide.topAnchor, right: nil, bottom: nil, left: contentView.safeAreaLayoutGuide.leftAnchor, padding: .init(top: 10, left: 10, bottom: 0, right: 0), size: .init(width: 100, height: 12))
         mainTitle.anchor(top: subTitle.safeAreaLayoutGuide.bottomAnchor, right: nil, bottom: nil, left: contentView.safeAreaLayoutGuide.leftAnchor, padding: .init(top: 5, left: 10, bottom: 0, right: 0), size: .init(width: 100, height: 40))
         imageIcon.anchor(top: mainTitle.bottomAnchor, right: contentView.safeAreaLayoutGuide.rightAnchor, bottom: nil, left: nil, padding: .init(top: 5, left: 0, bottom: 0, right: 20), size: .init(width: 30, height: 30))
     }
-    func configure(_ data: CellData) {
+    
+    func configure(with data: InquiryViewModel) {
         self.subTitle.text = data.subtitle
         self.mainTitle.text = data.mainTitle
         if !data.imageName.isEmpty {
-        self.imageIcon.image = UIImage(named: data.imageName)
+            self.imageIcon.image = UIImage(named: data.imageName)
         }
     }
 
-}
-extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor?,
-                right: NSLayoutXAxisAnchor?,
-                bottom: NSLayoutYAxisAnchor?,
-                left: NSLayoutXAxisAnchor?,
-                padding: UIEdgeInsets = .zero,
-                size: CGSize = .zero) {
-        translatesAutoresizingMaskIntoConstraints = false
-        if let top = top {
-            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
-        }
-        if let right = right {
-            rightAnchor.constraint(equalTo: right, constant: -padding.right).isActive = true
-        }
-        if let bottom = bottom {
-            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
-        }
-        if let left = left {
-            leftAnchor.constraint(equalTo: left, constant: padding.left).isActive = true
-        }
-        if size.width != 0 {
-            widthAnchor.constraint(equalToConstant: size.width).isActive = true
-        }
-        if size.height != 0 {
-            heightAnchor.constraint(equalToConstant: size.height).isActive = true
-        }
-    }
 }
