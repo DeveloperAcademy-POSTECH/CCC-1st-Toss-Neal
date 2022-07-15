5326817e94a6a750b6d4f1ac5ab22c8e269a3bd4
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-07-15 19:24:13 +0900

    [Feat] CollectionView Cell내부에 사용될 TableView 생성, HeaderView 필요

--- CCC-2nd-Toss-Neal/View/AssetCollectionViewCell.swift
+++ CCC-2nd-Toss-Neal/View/AssetCollectionViewCell.swift
@@ -7,49 +7,10 @@
 
 import UIKit
 
-//class AssetCollectionViewCell: UICollectionViewCell {
-//    static let identifier = "AssetCollectionViewCell"
-//    private let assetTable: UITableView = {
-//        let table = UITableView(frame: .zero, style: .plain)
-//        table.register(AssetTableViewCell.self, forCellReuseIdentifier: AssetTableViewCell.identifier)
-//        return table
-//    }()
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        initView()
-//    }
-//    required init?(coder: NSCoder) {
-//        fatalError()
-//    }
-//    func initView() {
-//        self.contentView.backgroundColor = .clear
-//        self.addSubview(assetTable)
-//        assetTable.frame = contentView.bounds
-//        assetTable.layer.cornerRadius = 15
-//
-//        assetTable.dataSource = self
-//        assetTable.delegate = self
-//    }
-//}
-//
-//extension AssetCollectionViewCell: UITableViewDataSource, UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 2
-//    }
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: AssetTableViewCell.identifier, for: indexPath) as? AssetTableViewCell else {
-//            return UITableViewCell()
-//        }
-//        return cell
-//    }
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 60
-//    }
-//}
-//
-
 class AssetCollectionViewCell: UICollectionViewCell {
-    static let identifier: String = "assetCollectionViewCell"
+    static let identifier = "AssetCollectionViewCell"
+    let dummyAsset: [Asset] = [Asset(imageName: "shinhan_symbol", assetType: "신한은행 계좌", deposit: 20024132, buttonStyle: "송금"), Asset(imageName: "shinhan_symbol", assetType: "저축예금", deposit: 20202020322, buttonStyle: "송금")]
+    
     private let assetTable: UITableView = {
         let table = UITableView(frame: .zero, style: .plain)
         table.register(AssetTableViewCell.self, forCellReuseIdentifier: AssetTableViewCell.identifier)
@@ -57,31 +18,54 @@ class AssetCollectionViewCell: UICollectionViewCell {
     }()
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.initView()
+        initView()
     }
-    required init(coder: NSCoder) {
-        fatalError("init error")
+    required init?(coder: NSCoder) {
+        fatalError()
     }
     func initView() {
         self.contentView.backgroundColor = .clear
-        self.contentView.addSubview(assetTable)
+        self.addSubview(assetTable)
+        setAssetTable()
+    }
+    private func setAssetTable() {
         assetTable.frame = contentView.bounds
         assetTable.layer.cornerRadius = 15
+        assetTable.separatorStyle = .none
+        
+        assetTable.dataSource = self
+        assetTable.delegate = self
+        
+        assetTable.translatesAutoresizingMaskIntoConstraints = false
+        
+        let assetTableConstraint = [
+            assetTable.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 10),
+            assetTable.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: 10),
+            assetTable.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
+            assetTable.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor)
+        ]
+        NSLayoutConstraint.activate(assetTableConstraint)
     }
 }
 
-extension AssetCollectionViewCell: UITableViewDelegate, UITableViewDataSource {
+extension AssetCollectionViewCell: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return dummyAsset.count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: AssetTableViewCell.identifier, for: indexPath) as? AssetTableViewCell else {
             return UITableViewCell()
         }
-        cell.textLabel?.text = "1111"
+        
+        let asset = dummyAsset[indexPath.row]
+        let model = AssetViewModel(imageName: asset.imageName, assetType: asset.assetType, deposit: asset.deposit, buttonStyle: asset.buttonStyle)
+        cell.configure(with: model)
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120
+        return 80
     }
 }
--- CCC-2nd-Toss-Neal/View/AssetTableViewCell.swift
+++ CCC-2nd-Toss-Neal/View/AssetTableViewCell.swift
@@ -7,50 +7,102 @@
 
 import UIKit
 
-// TODO: Title, 및, 데이터 
-//class AssetTableViewCell: UITableViewCell {
-//    static let identifier = "AssetTableViewCell"
-//
-//    private let titleLabel: UILabel = {
-//        let lbl = UILabel()
-//        lbl.translatesAutoresizingMaskIntoConstraints = false
-//        lbl.text = "fsmfdksmdfksmf"
-//        return lbl
-//    }()
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//    }
-//
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        titleLabel.frame = contentView.bounds
-//        contentView.addSubview(titleLabel)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
-//
-
-
+// TODO: Title, 및, 데이터
 
 class AssetTableViewCell: UITableViewCell {
-    static let identifier = "assetTableViewCell"
+    static let identifier = "AssetTableViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let bankTypeLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.systemFont(ofSize: 16, weight: .light)
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
+    
+    private let cashLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        return lbl
+    }()
+    
+    private let figureButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.gray()
+        config.titlePadding = 10.0
+        config.baseForegroundColor = .black.withAlphaComponent(0.9)
+        config.buttonSize = .medium
+        
+        button.configuration = config
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setLayoutConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setLayoutConstraints() {
+        [posterImageView, bankTypeLabel, cashLabel, figureButton].forEach { view in
+            self.contentView.addSubview(view)
+        }
+        
+        let posterImageViewConstraints = [
+            posterImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            posterImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            posterImageView.widthAnchor.constraint(equalToConstant: 48)
+        ]
+        
+        let bankTypeLabelConstraints = [
+            bankTypeLabel.leadingAnchor.constraint(equalTo: posterImageView.trailingAnchor, constant: 20),
+            bankTypeLabel.trailingAnchor.constraint(equalTo: figureButton.leadingAnchor, constant: -30),
+            bankTypeLabel.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 20)
+        ]
+        
+        let cashLabelConstraints = [
+            cashLabel.leadingAnchor.constraint(equalTo: bankTypeLabel.leadingAnchor),
+            cashLabel.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ]
+
+        let figureButtonConstraints = [
+            figureButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            figureButton.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant: -10)
+        ]
+        
+        NSLayoutConstraint.activate(posterImageViewConstraints)
+        NSLayoutConstraint.activate(bankTypeLabelConstraints)
+        NSLayoutConstraint.activate(cashLabelConstraints)
+        NSLayoutConstraint.activate(figureButtonConstraints)
+    }
+    
+    func configure(with model: AssetViewModel) {
+        posterImageView.image = UIImage(named: model.imageName)
+        bankTypeLabel.text = model.assetType
+        cashLabel.text = model.depositText
+        figureButton.setTitle(model.buttonStyle, for: .normal)
+        figureButton.isHidden = false
+    }
 }
