663bb498dfb4ac492b0710830b2a69e1cae12283
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-07-17 16:08:52 +0900

    [Add] UIScreen screenWidth 추가

---
+++ CCC-2nd-Toss-Neal/Resources/Extension/UIScreen+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIScreen+Extension.swift
+//  CCC-2nd-Toss-Neal
+//
+//  Created by yudonlee on 2022/07/15.
+//
+
+import UIKit
+
+extension UIScreen {
+    static let screenWidth: CGFloat = UIScreen.main.bounds.size.width
+}
